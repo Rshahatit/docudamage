2f47368b3a1081a924a5923c590ef63d15a5eca1
Raya Anderson <rayazand@gmail.com>
2020-02-16 12:52:45 -0800

    fixing forms to match UI

--- models/docu.model.js
+++ models/docu.model.js
@@ -5,12 +5,13 @@ const Schema = mongoose.Schema;
 const docuSchema = new Schema({
     name: { type: String, required: true},
     claim: { type: String, required: true},
-    address: { type: String, required: true},
+    streetaddress: { type: String, required: true},
+    city: { type: String, required: true},
+    state: { type: String, required: true},
     zipcode: { type: Number, required: true},
-    description: { type: String, required: true},
+    country: { type: String, required: true},
     cost: { type: Number, required: true},
     dateofaccident: { type: Date, required: true},
-
 }, {
     timestamps: true,
 });
--- routes/docu.js
+++ routes/docu.js
@@ -10,16 +10,21 @@ router.route('/').get((req, res) => {
 router.route('/add').post((req, res) => {
     const name = req.body.name;
     const claim = req.body.description;
-    const address = req.body.description;
+    const streetaddress = req.body.description;
+    const city = req.body.description;
+    const state = req.body.description;
+    const country = req.body.description;
     const zipcode = Number(req.body.duration);
-    const description = req.body.description;
     const cost = Number(req.body.duration);
     const dateofaccident = Date.parse(req.body.date);
 
     const newDocu = new Docu({
         name,
         claim,
-        address,
+        streetaddress,
+        city,
+        state,
+        country,
         zipcode,
         description,
         cost,
@@ -45,10 +50,13 @@ router.route('/:id').delete((req, res) => {
 
 router.route('/update/:id').post((req, res) => {
     Docu.findById(req.params.id)
-        .then(Docu => {
+        .then(docu => {
             docu.name = req.body.name;
             docu.claim = req.body.description;
-            docu.address = req.body.description;
+            docu.streetaddress = req.body.description;
+            docu.city = req.body.description;
+            docu.state = req.body.description;
+            docu.country = req.body.description;
             docu.zipcode = Number(req.body.duration);
             docu.description = req.body.description;
             docu.cost = Number(req.body.duration);
