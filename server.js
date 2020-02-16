c1c91b3fe819e46ce4562f045bc9eb323d187b6e
Raya Anderson <rayazand@gmail.com>
2020-02-15 19:34:55 -0800

    should be set for API

--- server.js
+++ server.js
@@ -9,13 +9,18 @@ const app = express()
 app.use(cors());
 app.use(express.json());
 
+
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {useNewUrlParser: true }
 );
-const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log("MongoDB databse connection establishes successfully");
-})
+const db = mongoose.connection;
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('connected to database'))
+
+const imagesRouter = require('./routes/images');
+app.use('/images', imagesRouter)
+const docuRouter = require('./routes/docu');
+app.use('/docu', docuRouter)
 
 
 app.listen(3000, () => console.log('server started'))
